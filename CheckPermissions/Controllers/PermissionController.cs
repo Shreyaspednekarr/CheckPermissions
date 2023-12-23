3412535c1b947c9369d37082cde2ef3a6850c655
Shreyaspednekarr <shreyaspednekar4@gmail.com>
2023-12-23 22:44:09 +0530

    Added endpoints

--- CheckPermissions/Controllers/PermissionController.cs
+++ CheckPermissions/Controllers/PermissionController.cs
@@ -13,5 +13,77 @@ namespace CheckPermissions.Controllers
         {
             _permissionService = permissionService ?? throw new ArgumentNullException(nameof(permissionService));
         }
+
+        [HttpGet("Get/{userId}")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(int userId)
+        {
+            try
+            {
+                await _permissionService.Get(userId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Create/{permissionName}/{roleId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(string permissionName, int roleId)
+        {
+            try
+            {
+                await _permissionService.Create(permissionName, roleId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Delete/{permissionId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int permissionId)
+        {
+            try
+            {
+                await _permissionService.Delete(permissionId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Assign/{permissionId}/{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Assign(int permissionId, int userId)
+        {
+            try
+            {
+                await _permissionService.Assign(permissionId, userId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
     }
 }
--- CheckPermissions/Controllers/RoleController.cs
+++ CheckPermissions/Controllers/RoleController.cs
@@ -13,5 +13,77 @@ namespace CheckPermissions.Controllers
         {
             _roleService = roleService ?? throw new ArgumentNullException(nameof(roleService));
         }
+
+        [HttpGet("Get/{userId}")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(int userId)
+        {
+            try
+            {
+                await _roleService.Get(userId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Create/{roleName}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(string roleName)
+        {
+            try
+            {
+                await _roleService.Create(roleName).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Delete/{roleId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int roleId)
+        {
+            try
+            {
+                await _roleService.Delete(roleId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("Assign/{roleId}/{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Assign(int roleId, int userId)
+        {
+            try
+            {
+                await _roleService.Assign(roleId, userId).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
     }
 }
